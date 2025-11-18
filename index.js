cb0032a3f665273fa9b9ee5ffdf6495c744076c3
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-11-18 16:38:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 function validateAge(oldEnough) {
 return document.getElementById('result').innerHTML = 'old enough';
-  else {document.getElementById('result').innerHTML = 'not old enough'};
-};
-
+}: else {document.getElementById('result').innerHTML = 'not old enough'};
